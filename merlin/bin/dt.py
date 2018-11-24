eeb3bd1b0a161e29696a68ebb746ee5b86174547
Peter Harrison <blackriver@simiya.com>
2018-11-24 03:14:29 -0800

    Added Random Forest evaluation.

--- merlin/bin/dt.py
+++ merlin/bin/dt.py
@@ -8,8 +8,10 @@ import time
 
 # PIP3 imports.
 import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+
+from sklearn import model_selection
+from sklearn.ensemble import RandomForestClassifier
 
 # Merlin imports
 from merlin import database
@@ -31,7 +33,7 @@ def main():
 
     # Get the data for processing
     data = database.DataDT(filename)
-    x_train, x_test, y_train, y_test = train_test_split(
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(
         data.vectors(),
         data.classes(),
         test_size=0.3,
@@ -41,8 +43,24 @@ def main():
     tree = DecisionTreeClassifier(criterion='entropy').fit(x_train, y_train)
 
     print(
-        '> The prediction accuracy is: {:.2f}%'.format(
+        '> The Decison Tree prediction accuracy is: {:.2f}%'.format(
             tree.score(x_test, y_test) * 100))
+
+    ################################################
+
+    '''print(y_train.flatten())
+    print(y_train.shape)
+    sys.exit()'''
+
+    num_trees = 100
+    max_features = 3
+    kfold = model_selection.KFold(n_splits=10)
+    model = RandomForestClassifier(n_estimators=num_trees, max_features=max_features)
+    results = model_selection.cross_val_score(model, data.vectors(), data.classes().flatten(), cv=kfold)
+
+    print(
+        '> The Random Forest prediction accuracy is: {:.2f}%'.format(
+            results.mean() * 100))
 
 
 if __name__ == "__main__":
